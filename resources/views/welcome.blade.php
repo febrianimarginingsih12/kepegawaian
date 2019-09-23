6259c0a999bb55adbcc66d4e89adecb396ad8844
febrianimarginingsih12 <febriani030202@gmail.com>
2019-09-23 13:20:29 +0700

    perubahan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    febriani marginingsih
                 </div>
 
                 <div class="links">
